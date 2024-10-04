8693a3512375b33e0f6603406aa02e9e7f112e0e
Lianne Turnbull <lianne.turnbull@gmail.com>
2024-10-04 16:49:56 +0100

    validate room codes

--- src/room/get-room.ts
+++ src/room/get-room.ts
@@ -4,14 +4,18 @@ import { createClient } from "redis";
 import Room from "./room";
 
 const getRoom = async (code: string) => {
-    const client = createClient();
-    client.on("error", (err) => console.log("Redis Client Error", err));
-    await client.connect();
-    const room = await client.get(code);
-    await client.disconnect();
-    if (room) {
-        return new Room(room);
+    const c = code.toUpperCase();
+    if (c.match(/^[A-Z]{4}$/)) {
+        const client = createClient();
+        client.on("error", (err) => console.log("Redis Client Error", err));
+        await client.connect();
+        const room = await client.get(c);
+        await client.disconnect();
+        if (room) {
+            return new Room(room);
+        }
     }
+    return null;
 };
 
 export default getRoom;
